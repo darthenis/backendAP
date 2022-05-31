2de816f312402ed6c7473643abb7d9d456f349fe
Emiliano Acevedo <emi.acevedo.letras@gmail.com>
2022-05-31 14:51:03 -0300

    update

--- src/main/java/portfolios/security/MainSecurity.java
+++ src/main/java/portfolios/security/MainSecurity.java
@@ -100,7 +100,7 @@ public class MainSecurity extends WebSecurityConfigurerAdapter {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedOrigins(Arrays.asList("https://portfolioap-102b7.web.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "PUT", "POST","OPTIONS", "DELETE"));
         configuration.setAllowedHeaders(Arrays.asList("authorization","content-type"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
